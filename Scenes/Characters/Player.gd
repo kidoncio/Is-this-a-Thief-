8df0ad041200bd10612eec2d489bb38db698e31e
Kidoncio <lucas@lapou.com.br>
2019-06-17 21:50:47 -0300

    Add update_motion, com a movimentação do Player

--- Scenes/Characters/Player.gd
+++ Scenes/Characters/Player.gd
@@ -8,4 +8,18 @@ func _process(delta):
 
 
 func update_motion(delta: float) -> void:
-	pass
+	look_at(get_global_mouse_position())
+	
+	if Input.is_action_pressed("ui_up") and not Input.is_action_pressed("ui_down"):
+		motion.y = clamp((motion.y - SPEED), - MAX_SPEED, 0)
+	elif Input.is_action_pressed("ui_down") and not Input.is_action_pressed("ui_up"):
+		motion.y = clamp((motion.y + SPEED), 0, MAX_SPEED)
+	else:
+		motion.y = lerp(motion.y, 0, FRICTION)
+	
+	if Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
+		motion.x = clamp((motion.x - SPEED), - MAX_SPEED, 0)
+	elif Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
+		motion.x = clamp((motion.x + SPEED), 0, MAX_SPEED)
+	else:
+		motion.x = lerp(motion.x, 0, FRICTION)
