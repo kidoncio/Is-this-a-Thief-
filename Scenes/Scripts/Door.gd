2a427259161155393926723025f5f1e61aad18e3
Kidoncio <lucas@lapou.com.br>
2019-06-29 00:32:43 -0300

    Ajusta bug nas LockedDoor

--- Scenes/Scripts/Door.gd
+++ Scenes/Scripts/Door.gd
@@ -8,23 +8,22 @@ const OPEN_ANIMATION: String = "open"
 const CLOSE_ANIMATION: String = "close"
 
 func _on_Door_body_entered(body):
-	if body != Global.Player && !is_open:
+	if body != Global.Player:
 		open()
 	else:
 		player_can_open = true
 
 
 func open() -> void:
-	if $AnimationPlayer.is_playing():
+	if $AnimationPlayer.is_playing() || is_open:
 		return
-	
-	is_open = true
 	
 	$AnimationPlayer.play(OPEN_ANIMATION)
 
 
 func close() -> void:
-	is_open = false
+	if $AnimationPlayer.is_playing() || !is_open:
+		return
 	
 	$AnimationPlayer.play(CLOSE_ANIMATION)
 
@@ -40,3 +39,10 @@ func _on_Door_body_exited(body):
 func _input_event(viewport, event, shape_idx):
 	if player_can_open && Input.is_mouse_button_pressed(BUTTON_LEFT):
 		open()
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == 'close':
+		is_open = false
+	else:
+		is_open = true
